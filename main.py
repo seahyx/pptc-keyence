7152fc3eb5d21ee95c840aff7c18a59d74d56dbe
Seah Ying Xiang <seahyx@gmail.com>
2019-12-26 15:36:56 +0800

    Added gitignore

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('home.html')
+
+@app.route('/open-door/')
+def api():
+	return jsonify(result='hello')
---
+++ static/home.js
@@ -0,0 +1,16 @@
+function getHttp(url) {
+	return new Promise((resolve) => {
+		let xhttp = new XMLHttpRequest()
+	
+		xhttp.onreadystatechange = function() {
+			if (this.readyState === 4 && this.status === 200) {
+			resolve(this.responseText)
+			}
+		}
+	
+		xhttp.open("GET", url)
+		xhttp.send();
+	})
+}
+
+document.querySelector("#button-open").addEventListener('click', () => getHttp('/open-door/'))
