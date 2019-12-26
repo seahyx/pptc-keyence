51fc9fbd1cbef57634701b69fc69441d06210350
Seah Ying Xiang <seahyx@gmail.com>
2019-12-26 15:42:08 +0800

    Added wiringpi

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template, request, jsonify
+import wiringpi
+import time
 
 app = Flask(__name__)
+
+# Wiring pi setup
+wiringpi.wiringPiSetup()
+
+wiringpi.pinMode(4, 1)
 
 @app.route('/')
 def index():
@@ -8,4 +15,11 @@ def index():
 
 @app.route('/open-door/')
 def api():
-	return jsonify(result='hello')
+	open_door()
+	return jsonify(result='success')
+
+
+def open_door():
+	wiringpi.digitalWrite(4, 1)
+	time.sleep(500)
+	wiringpi.digitalWrite(4, 0)
