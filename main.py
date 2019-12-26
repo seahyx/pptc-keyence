59b6c6945d6fef25e3ae6dd51783645fa3d23787
Seah Ying Xiang <seahyx@gmail.com>
2019-12-26 16:00:02 +0800

    Allowed server to run by executing script directly

--- main.py
+++ main.py
@@ -23,3 +23,11 @@ def open_door():
 	wiringpi.digitalWrite(4, 1)
 	time.sleep(500)
 	wiringpi.digitalWrite(4, 0)
+
+
+# If we're running this script directly, this portion executes. The Flask
+#  instance runs with the given parameters. Note that the "host=0.0.0.0" part
+#  is essential to telling the system that we want the app visible to the 
+#  outside world.
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
