08c8df87eca12b802596c0016d52de28ca3bc570
Seah Ying Xiang <seahyx@gmail.com>
2019-12-28 20:31:33 +0800

    Added support for extra button

--- static/home.js
+++ static/home.js
@@ -13,5 +13,9 @@ function getHttp(url) {
 	})
 }
 
-document.querySelector("#button-open").addEventListener('click', () => getHttp('/open-door/'))
-document.querySelector("#button-open").addEventListener('click', () => console.log('Button Pressed'))
+let btn_open = document.querySelector("#btn-open");
+let btn_open_hold = document.querySelector("#btn-open-hold");
+btn_open.addEventListener('click', () => getHttp('/open-door/'))
+btn_open.addEventListener('click', () => console.log('Button Pressed'))
+btn_open_hold.addEventListener('click', () => getHttp('/open-door/'))
+btn_open_hold.addEventListener('click', () => console.log('Button Pressed'))
