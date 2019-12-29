53af3afc674aefe50997da50158171eb79958ac1
Seah Ying Xiang <seahyx@gmail.com>
2019-12-29 20:57:16 +0800

    Converted home.js to typescript

---
+++ static/src/ts/home.ts
@@ -0,0 +1,21 @@
+function getHttp(url: string): Promise<any> {
+	return new Promise((resolve: Function) => {
+		let xhttp = new XMLHttpRequest()
+	
+		xhttp.onreadystatechange = function() {
+			if (this.readyState === 4 && this.status === 200) {
+			resolve(this.responseText)
+			}
+		}
+	
+		xhttp.open("GET", url)
+		xhttp.send();
+	})
+}
+
+let btn_open: HTMLElement = document.getElementById('btn_open');
+let btn_open_hold: HTMLElement = document.getElementById('btn-open-hold');
+btn_open.addEventListener('click', () => getHttp('/open-door/'));
+btn_open.addEventListener('click', () => console.log('Button Pressed'));
+btn_open_hold.addEventListener('click', () => getHttp('/open-door/'));
+btn_open_hold.addEventListener('click', () => console.log('Button Pressed'));
