0fea821ba6d46170b46a31bb51840a3c633e020e
Seah Ying Xiang <seahyx@gmail.com>
2019-12-30 01:39:14 +0800

    Fixed spelling error

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-class LoginForm(Flaskform):
+class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	rmb_me = BooleanField('Remember Me')
