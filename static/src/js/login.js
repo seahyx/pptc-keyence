10180369cee47ebd6a602dc08e4b60032f82455b
Seah Ying Xiang <seahyx@gmail.com>
2019-12-29 23:59:32 +0800

    Created login page script

---
+++ static/src/js/login.js
@@ -0,0 +1,5 @@
+"use strict";
+let in_user = document.getElementById('username');
+let in_pass = document.getElementById('password');
+let in_rmbme = document.getElementById('remember');
+let btn_submit = document.getElementById('submit');
---
+++ static/src/ts/login.ts
@@ -0,0 +1,4 @@
+let in_user: HTMLElement = document.getElementById('username');
+let in_pass: HTMLElement = document.getElementById('password');
+let in_rmbme: HTMLElement = document.getElementById('remember');
+let btn_submit: HTMLElement = document.getElementById('submit');
