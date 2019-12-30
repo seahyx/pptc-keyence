29f8e2c606ae8da234f1a30c5793a6fbe4a0d9a8
Seah Ying Xiang <seahyx@gmail.com>
2019-12-30 22:03:27 +0800

    Added user login system

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, jsonify, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
 import time
 # TODO: remove comments when merging
 # import wiringpi
@@ -17,13 +19,30 @@ def index():
 
 @app.route('/login/', methods = ['GET', 'POST'])
 def login():
+
+	# If user is logged in and navigates to this page somehow
+	if current_user.is_authenticated:
+		# Redirect back to home page
+		return redirect(url_for('index'))
+
 	form = LoginForm()
 
 	if form.validate_on_submit():
-		flash('Login required for user {}, remember me = {}'.format(form.username.data, form.rmb_me.data))
+
+		# Find a user by username from the User db table
+		user = User.query.filter_by(username = form.username.data).first()
+
+		if user is None or not user.check_password(form.password.data):
+			# Wrong username or password
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+
+		# Correct username and password
+		flash('Logged in successfully')
+		login_user(user, remember = form.rmb_me.data)
+
 		return redirect(url_for('index'))
 	
-	flash('Login required for user {}, remember me = {}'.format(form.username.data, form.rmb_me.data))
 	return render_template('login.html', title = 'Login', form = form)
 
 @app.route('/registration/')
