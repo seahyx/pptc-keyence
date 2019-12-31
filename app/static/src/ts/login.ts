5e95c79be58a5a2c123395bc5c9ec34d1866d554
Seah Ying Xiang <seahyx@gmail.com>
2019-12-31 18:58:03 +0800

    Removed unnecessary script files

--- app/static/src/ts/login.ts
+++
@@ -1,4 +0,0 @@
-let in_user: HTMLElement = document.getElementById('username');
-let in_pass: HTMLElement = document.getElementById('password');
-let in_rmbme: HTMLElement = document.getElementById('remember');
-let btn_submit: HTMLElement = document.getElementById('submit');
