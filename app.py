7ce3c562d009d38b959265e9f50e415cc1c564d4
Seah Ying Xiang <seahyx@gmail.com>
2020-01-01 03:25:42 +0800

    Moved startup run code to app.py

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ from app.models import User
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User}
+
+#  If we're running this script directly, this portion executes. The Flask
+#  instance runs with the given parameters. Note that the "host=0.0.0.0" part
+#  is essential to telling the system that we want the app visible to the 
+#  outside world.
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
--- app/routes.py
+++ app/routes.py
@@ -154,11 +154,3 @@ def api():
 		return jsonify(message='success')
 	else:
 		return jsonify(message='none')
-	
-
-#  If we're running this script directly, this portion executes. The Flask
-#  instance runs with the given parameters. Note that the "host=0.0.0.0" part
-#  is essential to telling the system that we want the app visible to the 
-#  outside world.
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
