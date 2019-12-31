4054701fb19837a6449a6e07ba74374ae6e67f61
Seah Ying Xiang <seahyx@gmail.com>
2019-12-31 21:21:59 +0800

    Added handling for removal of accounts

--- app/routes.py
+++ app/routes.py
@@ -85,7 +85,7 @@ def registration():
 		# Netloc tests if next is pointed towards other site, which can link to malicious sites. Thus not accepting the redirect if it has value.
 		if not next_page or url_parse(next_page).netloc != '':
 			next_page = url_for('index')
-			
+
 		return redirect(next_page)
 
 	return render_template('registration.html', title='Create new account', form=form)
@@ -95,7 +95,30 @@ def registration():
 @login_required
 @permissions.admin_required
 def dashboard():
-	return render_template('dashboard.html', title='Admin Dashboard', users=User.query.all())
+	if request.args.get('rmId') and request.args.get('rmId').isdigit():
+		removal_id = int(request.args.get('rmId'))
+		print('Dashboard: Account of list id {} requested'.format(removal_id))
+		
+		# Now check if the number is valid and that the user is safe to delete
+		user = User.query.filter_by(id=removal_id).first()
+		if (user):
+			# The user exists
+			if ((user.id != current_user.id) and user.account_type != 0):
+				# The user is not the currently logged in user or root, thus can be safely deleted
+				db.session.delete(user)
+				db.session.commit()
+				print('Success: User with username {}, id of {} is deleted'.format(user.username, removal_id))
+				flash('Success: User with username {}, id of {} is deleted'.format(user.username, removal_id))
+			else:
+				# The user is root or current user, thus cannot be removed
+				print('Error: User with username {}, id of {} cannot be deleted'.format(user.username, removal_id))
+				flash('Error: User with username {}, id of {} cannot be deleted'.format(user.username, removal_id))
+		else:
+			# The user doesn't exist
+			print('Error: User with id of {} does not exist'.format(removal_id))
+			flash('Error: User with id of {} does not exist'.format(removal_id))
+
+	return render_template('dashboard.html', title='Admin Dashboard', users=User.query.order_by(User.account_type).order_by(User.username).all())
 
 @app.route('/open-door/')
 @login_required
