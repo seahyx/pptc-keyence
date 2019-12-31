f487856f2480f341d2e609458d8c07d3f6d6d866
Seah Ying Xiang <seahyx@gmail.com>
2019-12-31 21:22:28 +0800

    Added get request to send to remove accounts

--- app/static/src/js/dashboard.js
+++ app/static/src/js/dashboard.js
@@ -1,21 +1,9 @@
-function getHttp(url) {
-    return new Promise((resolve) => {
-        let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
-                resolve(this.responseText);
-            }
-        };
-        xhttp.open("GET", url);
-        xhttp.send();
-    });
-}
+
 let btns_remove = document.getElementsByClassName('button-remove');
-let btns_edit = document.getElementsByClassName('button-edit');
+let btns_reset = document.getElementsByClassName('button-reset');
+
 for (let i = 0; i < btns_remove.length; i++) {
-    const url = new URLSearchParams();
-    url.set('rmId', i.toString());
     btns_remove[i].addEventListener('click', () => {
-        getHttp('/dashboard/?' + url.toString());
-    });
+		window.location.href = window.location.pathname +'?' + 'rmId=' + btns_remove[i].id.toString();
+	});
 }
