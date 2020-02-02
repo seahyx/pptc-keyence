ede59f7eac54fda2154e2a74a1b6427ec2b57743
Seah Ying Xiang <seahyx@gmail.com>
2020-02-02 14:19:24 +0800

    Removed unrelated personnel

--- config.py
+++ config.py
@@ -14,4 +14,4 @@ class Config(object):
 	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	ADMINS = ['seahyx@gmail.com', 'seahyinghang8@gmail.com', 'seahyw@gmail.com']
+	ADMINS = ['seahyx@gmail.com', 'seahyw@gmail.com']
