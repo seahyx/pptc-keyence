99a590edbec6464680121b37b5e7fdb4ab16cbdc
Seah Ying Xiang <seahyx@gmail.com>
2020-02-07 19:44:07 +0800

    Added system time to statusbar

---
+++ app/static/src/js/status-bar.js
@@ -0,0 +1,13 @@
+display_time();
+
+function display_load() {
+	let refresh = 1000; // Refresh rate in milliseconds
+	mytime = setTimeout('display_time()', refresh);
+}
+
+function display_time() {
+	let date = new Date();
+	let formatted_date = `${date.toLocaleTimeString()} - ${date.toLocaleDateString()}`;
+	document.getElementById('time').innerHTML = formatted_date;
+	display_load();
+}
