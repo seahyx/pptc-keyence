fb02702b4efda92fd0d2d430d576f3ef75d7f5b9
Seah Ying Xiang <seahyx@gmail.com>
2020-02-07 16:10:28 +0800

    Updated descriptions for failed validators

--- app/forms.py
+++ app/forms.py
@@ -14,16 +14,16 @@ class RegistrationForm(FlaskForm):
 
 	password = PasswordField('Password', validators=[DataRequired('Password is required, please'), Length(min=4, message='Password must be at least %(min)d characters long')])
 
-	password2 = PasswordField('Confirm Password', validators=[DataRequired('Please confirm the password'), EqualTo('password', 'Passwords must match')])
+	password2 = PasswordField('Confirm Password', validators=[DataRequired('Please confirm the password'), EqualTo('password', 'Passwords must match, please try again')])
 
-	account_type = SelectField('Account Type', choices=User.ACCOUNT_TYPES[1:len(User.ACCOUNT_TYPES)], coerce=int, default=3)
+	account_type = SelectField('Account Type', choices=User.ACCOUNT_TYPES[1:len(User.ACCOUNT_TYPES)], coerce=int, default=2)
 
 	submit = SubmitField('Register')
 
 	def validate_username(self, username):
 		user = User.query.filter_by(username=username.data).first()
 		if user is not None:
-			raise ValidationError('This username has been taken')
+			raise ValidationError('This username has been taken, please choose another one')
 
 class ChangePasswordForm(FlaskForm):
 	password = PasswordField('Password', validators=[DataRequired('Password is required'), Length(min=4, message='Password must be at least %(min)d characters long')])
