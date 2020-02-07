24e39c95a3890553d7e027b9aacbf94c597afc66
Seah Ying Xiang <seahyx@gmail.com>
2020-02-07 22:18:24 +0800

    Added config for flask sessions

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+from os import path, environ
+basedir = path.abspath(path.dirname(__file__))
 
 class Config(object):
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+	SECRET_KEY = environ.get('SECRET_KEY') or 'you-will-never-guess'
 
 	# Database
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or 'sqlite:///' + path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+	# Flask-Session
+	SESSION_TYPE = environ.get('SESSION_TYPE') or 'sqlalchemy'
+
 	# Email
-	MAIL_SERVER = os.environ.get('MAIL_SERVER')
-	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	MAIL_SERVER = environ.get('MAIL_SERVER')
+	MAIL_PORT = int(environ.get('MAIL_PORT') or 25)
+	MAIL_USE_TLS = environ.get('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
 	ADMINS = ['seahyx@gmail.com', 'seahyw@gmail.com']
