d23e6a7c6a039da08a225c9343ee4ada271908ba
Seah Ying Xiang <seahyx@gmail.com>
2020-02-07 22:19:17 +0800

    Open and close session requests for navbar

--- app/static/src/js/status-bar.js
+++ app/static/src/js/status-bar.js
@@ -1,3 +1,5 @@
+// Clock
+
 display_time();
 
 function display_load() {
@@ -11,3 +13,29 @@ function display_time() {
 	document.getElementById('time').innerHTML = formatted_date;
 	display_load();
 }
+
+
+// Navbar open/close state
+
+function getHttp(url) {
+    return new Promise((resolve) => {
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                resolve(this.responseText);
+            }
+        };
+        xhttp.open("GET", url);
+        xhttp.send();
+    });
+}
+
+let nav_btn = document.getElementById('nav-btn');
+nav_btn.addEventListener('change', () => {
+	// If checked, navbar is closed
+	if (nav_btn.checked) {
+		getHttp('/navbar/closed')
+    } else {
+		getHttp('/navbar/open')
+    }
+});
