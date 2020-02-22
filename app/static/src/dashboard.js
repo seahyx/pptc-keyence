809890de6cf8449c2ed737a8c2bfbf1036797866
Seah Ying Xiang <seahyx@gmail.com>
2020-02-22 15:24:01 +0800

    Added confirmation to account deletion

--- app/static/src/dashboard.js
+++ app/static/src/dashboard.js
@@ -4,7 +4,12 @@ let btns_reset = document.getElementsByClassName('btn-js-reset');
 
 for (let i = 0; i < btns_remove.length; i++) {
 	btns_remove[i].addEventListener('click', () => {
-		window.location.href = window.location.pathname +'?' + 'rmId=' + btns_remove[i].id;
+
+		let conf_state = confirm('Are you sure you want to delete this account? This action cannot be reversed.')
+		
+		if (conf_state === true) {
+			window.location.href = window.location.pathname +'?' + 'rmId=' + btns_remove[i].id;
+		}
 	});
 	btns_reset[i].addEventListener('click', () => {
 		window.location.href = window.location.pathname + 'change-pass/' + btns_reset[i].id;
