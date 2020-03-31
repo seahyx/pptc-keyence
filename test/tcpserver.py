e77b030d6b5f89867a7064947d5d64155fa3bdfc
seahyw <30467130+seahyw@users.noreply.github.com>
2020-03-31 16:18:29 +0800

    Add files via upload

--- test/tcpserver.py
+++ test/tcpserver.py
@@ -5,7 +5,7 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 10000)
+server_address = ('localhost', 8500)
 print (f'starting up on {server_address[0]} port {server_address[1]}', file=sys.stderr)
 sock.bind(server_address)
 
@@ -31,10 +31,10 @@ while True:
 
 			if data:
 				if (message[0] == 'T1'):
-					data = b'T1,0,MSG-1001,1,,0,MSG-1002,0,MSG-1003'
-				elif (message[0] == 'N0'):
-					data = b'N0'
-				elif (message[0] == 'PW'):
+					data = b'T1,0,MS3092555-RMF,0,TG2003637-RMF,0,MS3007019-TMP,0,TG2003671-RMF,0,TG2003667-RMF,0,TG2003626-RMF,0,TG2003657-RMF,0,TG2003660-RMF,0,MS6754129-LMX2,0,TG2003642-RMF,0,MS2929572-AMS1,0,MS6999347-LMX1,0,MS6324325-NULL,0,MS5357075-PW1,0,MS3085936-LPM,0,MS3247197-HP11,0,MS6262931-NULL,0,MS5342413-PW1,0,TG2003635-RMF,0,TG2003630-RMF,0,MS3040982-HP12,0,TG2003661-RMF,0,MS6675922-LDR,0,TG2003640-RMF'
+				elif (message[0] == 'R0'):
+					data = b'R0'
+				elif (message[0][:2] == 'PW'):
 					data = b'PW'
 				elif (message[0] == 'exit'):
 					toExit = True
