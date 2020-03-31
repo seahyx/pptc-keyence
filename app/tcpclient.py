03be4a777a63664870ef455d8ff259aa5aa59b2c
Seah Ying Xiang <seahyx@gmail.com>
2020-04-01 03:27:54 +0800

    Added return message received from tcp server Improved class documentation

--- app/tcpclient.py
+++ app/tcpclient.py
@@ -1,13 +1,26 @@
+import threading
 import socket
 import sys
 
 class TCPClient:
+	''' TCP interface for client
+
+	This class will connect to the Vision TCP server/dummy test server
+	in the background. It will initialize with the server address and port
+	as configured in the config file.
+
+	:param app: The flask application instance.
+
+	:param address: IP address of the tcp server. Defaults to ``'localhost'``.
+
+	:param port: Port number of the tcp server. Defaults to ``8500``.
+	'''
 
 	SERVER_ADDRESS = None
 	sock = None
 	app = None
 
-	def __init__(self, app, address, port):
+	def __init__(self, app, address='localhost', port=8500):
 
 		# Get app
 		self.app = app
@@ -16,35 +29,52 @@ class TCPClient:
 		self.SERVER_ADDRESS = (address, port)
 
 	def connect(self):
-		# Connect the socket to the port where the server is listening
+		''' Connect the socket to the port where the server is listening '''
+
 		try:
-			self.app.logger.info(f'Connecting to {self.SERVER_ADDRESS[0]}, port {self.SERVER_ADDRESS[1]}')
+			self.app.logger.info(f'Attempting connection to {self.SERVER_ADDRESS[0]}, port {self.SERVER_ADDRESS[1]}')
 			self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			self.sock.connect(self.SERVER_ADDRESS)
 			self.app.logger.info(f'Connected to {self.SERVER_ADDRESS[0]}, port {self.SERVER_ADDRESS[1]}')
 			return True
+
 		except:
 			self.app.logger.info(f'Connection failed')
 			return False
 
 	def send(self, message):
+		''' Send message
+
+		:type message: str
+		:param message: Message to be sent
+		'''
 
 		# Connect to server
 		connected = self.connect()
 		if not connected:
-			return False
+			return None
+
+		data_decoded = ['']
 
 		try:
 			# Send data
-			self.app.logger.info(f'Sending "{message}" to {self.SERVER_ADDRESS[0]}, port {self.SERVER_ADDRESS[1]}')
+			self.app.logger.info(f'Sending "{message}" to server')
 			self.sock.sendall(str.encode(message))
 
 			# Look for the response
-			data = self.sock.recv(1024).decode('utf-8')
-			self.app.logger.info(f"Received \"{data}\" from {self.SERVER_ADDRESS[0]}, port {self.SERVER_ADDRESS[1]}")
+			data = self.sock.recv(1024)
+			
+			# Response is received
+			data_decoded = data.decode('utf-8').split(',')
+			self.app.logger.info(f'Received {data_decoded} from server')
+
 		except:
+
 			self.app.logger.warning(f'Failed to send "{message}" to {self.SERVER_ADDRESS[0]}, port {self.SERVER_ADDRESS[1]}')
+
 		finally:
-			self.app.logger.info(f'Closing socket to {self.SERVER_ADDRESS[0]}, port {self.SERVER_ADDRESS[1]}')
+
 			self.sock.close()
-			return True
+			self.app.logger.info(f'Closed client socket')
+			
+			return data_decoded
