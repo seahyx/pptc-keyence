e1dfdc7a27d42826bf39caa2d9e1be944575c72d
Seah Ying Xiang <seahyx@gmail.com>
2020-04-08 23:53:09 +0800

    Validate laser instrument on confirm

--- app/static/src/laser.js
+++ app/static/src/laser.js
@@ -32,6 +32,8 @@ btn_select_cancel.addEventListener('click', (event) => {
 
 var socketio = io.connect(`http://${document.domain}:${location.port}/laser/api`);
 
+// Start button, validate work order/part number
+
 btn_start.addEventListener('click', () => {
 
 	if (!in_work_order.value || !in_part_number.value) {
@@ -40,21 +42,39 @@ btn_start.addEventListener('click', () => {
 	}
 
 	laser_modal.setAttribute('data-enabled', '');
-	socketio.emit('start', {data: `${in_work_order.value},${in_part_number.value}`})
+	socketio.emit('start', in_work_order.value, in_part_number.value)
 
 });
+
+// Confirm button
 
 btn_select_confirm.addEventListener('click', () => {
-	
+
+	// Change laser instrument selected
 	let instrument_selected = document.querySelector('input[type="radio"][name="laser-modal-radio"]:checked');
+
 	if (instrument_selected !== null) {
+
 		td_laser_select.innerText = instrument_selected.value;
+
+	} else {
+
+		// No instrument selected (error!)
+		alert('Please select a laser instrument!')
+		return;
+
 	}
+	
+	// Hide Modal
 	laser_modal.removeAttribute('data-enabled');
 
-	socketio.emit('confirm', {data: `${in_work_order.value},${in_part_number.value}`});
+	// Send data to server
+	socketio.emit('confirm', in_work_order.value, in_part_number.value, instrument_selected.value);
 
 });
+
+
+// Responses
 
 socketio.on('alert', function(msg) {
 	alert(msg);
@@ -67,7 +87,6 @@ socketio.on('response', function(msg) {
 socketio.on('1d_barcode', function(msg) {
 	console.log(`Received data from event '1d_barcode': ${msg.data}`);
 	laser_rack_id.innerText = msg.data;
-
 });
 
 socketio.on('confirm_data', function(msg) {
