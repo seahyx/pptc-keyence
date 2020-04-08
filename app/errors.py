32c86705152beeb7efc72c49b8bc75d422d6f8fe
Seah Ying Xiang <seahyx@gmail.com>
2020-04-08 21:33:51 +0800

    Updated logging info

--- app/errors.py
+++ app/errors.py
@@ -3,11 +3,11 @@ from app import app, db
 
 @app.errorhandler(404)
 def not_found_error(error):
-	app.logger.warning('404 Resource Not Found')
+	app.logger.warning(error)
 	return render_template('404.html', title='404 This is a sad day', no_header=True), 404
 
 @app.errorhandler(500)
 def internal_error(error):
 	db.session.rollback()
-	app.logger.warning('500 Internal Server Error')
+	app.logger.warning(error)
 	return render_template('500.html', title='500 This is a sad day', no_header=True), 500
