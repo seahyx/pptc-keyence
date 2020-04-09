4436241fba7683f75c6b4e758de24228078615ef
Seah Ying Xiang <seahyx@gmail.com>
2020-04-09 11:39:59 +0800

    Added better documentation

--- app/serialClient.py
+++ app/serialClient.py
@@ -6,12 +6,53 @@ import queue
 
 
 class SerialClient:
+
+	''' Serial interface to barcode scanner
+
+	This class will attempt a serial connection to the specified port on the terminal. Can be used as follows::
+
+		from flask import Flask
+		from serialclient import SerialClient
+
+		app = Flask(__name__)
+
+		# Init and attempt connection to com3
+		plc_ser = SerialClient(app, "com3")
+
+		# Send data
+		plc_ser.send_data('R')
+
+		# Check if data is ready
+		if plc_ser.data_ready:
+
+			# Retrieve data
+			data = plc_ser.get()
+			print(repr(data))
+
+	A loop may be required to check if the data is ready before retrieving it.
+
+	:param app: The flask application instance.
+
+	:param port: The port to connect to.
+
+	:param baudrate: Baud rate of the connection. Defaults to ``9600``.
+
+	:param parity: Parity of the connection. Defaults to ``serial.PARITY_EVEN``.
+
+	:param stopbits: Stop bits of the connection. Defaults to ``serial.STOPBITS_ONE``.
+
+	:param bytesize: Size of a byte. Defaults to ``serial.EIGHTBITS``.
+
+	:param timeout: Connection timeout. Defaults to ``1``.
+
+	'''
 	
 	def __init__(self, app, port, baudrate=9600, parity=serial.PARITY_EVEN, 
 				stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1):
 		self.app = app
-		self.dataready = False
+		self.data_ready = False
 		self.data = None
+		self.ser = None
 
 		self.app.logger.info(f'Attempting open to port {port}')
 
@@ -39,12 +80,9 @@ class SerialClient:
 
 			self.app.logger.info(f'Failed to open port {port}')
 			return
-
-	def dataReady(self):
-		return self.dataready
 		
 	def get(self):
-		self.dataready = False
+		self.data_ready = False
 		return self.data
 		
 	def send_data(self, data):
@@ -74,7 +112,7 @@ class SerialClient:
 		
 		self.app.logger.info(f'Received data {repr(data)}')
 		self.data = data
-		self.dataready = True
+		self.data_ready = True
 		
 	def read_from_port(self):
 		self.app.logger.info(f'Reading serial from port')
