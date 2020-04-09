67102d760ff4f729909873479cf2e8216a437a8d
Seah Ying Xiang <seahyx@gmail.com>
2020-04-09 09:47:13 +0800

    Updated documentation

--- app/permissions.py
+++ app/permissions.py
@@ -9,6 +9,19 @@ class PermissionsManager():
 	redirect_view = ''
 
 	def admin_required(self, func):
+		''' Restricts user access to administrative functions of the app.
+		
+		This decorator checks if the user is authenticated and has admin rights before allowing
+		them to access the page/link in question.
+
+		If there is unauthorized access, a warning message will be flashed and logged. Then, the
+		user will be redirected to the value of redirect_view that was set beforehand.
+
+		:param func: The view function to decorate
+		:param type: function
+		
+		'''
+
 		@wraps(func)
 		def wrapper(*args, **kwargs):
 			if current_user.is_authenticated and current_user.has_admin_rights():
