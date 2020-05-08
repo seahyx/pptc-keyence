704e231177ebc544161f2c18e1265c041cb512ac
Seah Ying Xiang <seahyx@gmail.com>
2020-05-08 16:04:25 +0800

    Whitespace adjustment

--- configfile.py
+++ configfile.py
@@ -50,22 +50,22 @@ class ConfigFile():
 		self.cartridge_assembly_QC_config = ConfigParser()
 		self.cartridge_assembly_QC_config.read(cartridge_assembly_QC_file)
 
-		self.laser_etch_QC 					= []
-		self.laser_etch_QC 					= {'Prefix': self.laser_etch_QC_config.get('PREFIX', 'Allowed Prefixes').split(';')}
-		self.laser_etch_QC['Instrument'] 	= self.laser_etch_QC_config.get('LASER INSTRUMENT', 'Instrument').split(';')
-		self.laser_etch_QC['WOLength'] 		= self.laser_etch_QC_config.getint('WORK ORDER', 'Length')
-		self.laser_etch_QC['PNLength'] 		= self.laser_etch_QC_config.getint('PART NUMBER', 'Length')
-		self.laser_etch_QC['PNFile'] 		= self.laser_etch_QC_config.get('PATH', 'PN Data Lookup')
-		self.laser_etch_QC['LogFile'] 		= self.laser_etch_QC_config.get('PATH', 'Log File')
-		self.laser_etch_QC['ImageDir'] 		= self.laser_etch_QC_config.get('PATH', 'Image Dir')
+		self.laser_etch_QC               = []
+		self.laser_etch_QC               = {'Prefix': self.laser_etch_QC_config.get('PREFIX', 'Allowed Prefixes').split(';')}
+		self.laser_etch_QC['Instrument'] = self.laser_etch_QC_config.get('LASER INSTRUMENT', 'Instrument').split(';')
+		self.laser_etch_QC['WOLength']   = self.laser_etch_QC_config.getint('WORK ORDER', 'Length')
+		self.laser_etch_QC['PNLength']   = self.laser_etch_QC_config.getint('PART NUMBER', 'Length')
+		self.laser_etch_QC['PNFile']     = self.laser_etch_QC_config.get('PATH', 'PN Data Lookup')
+		self.laser_etch_QC['LogFile']    = self.laser_etch_QC_config.get('PATH', 'Log File')
+		self.laser_etch_QC['ImageDir']   = self.laser_etch_QC_config.get('PATH', 'Image Dir')
 
 		# app.logger.info(f'Loading Laser Etch QC config: {self.laser_etch_QC}')
 
-		self.cartridge_assembly_QC = []
-		self.cartridge_assembly_QC = {'Prefix': self.cartridge_assembly_QC_config.get('PREFIX', 'Allowed Prefixes').split(';')}
-		self.cartridge_assembly_QC['LogFile'] 		= self.cartridge_assembly_QC_config.get('PATH', 'Log File')
-		self.cartridge_assembly_QC['ImageDir'] 		= self.cartridge_assembly_QC_config.get('PATH', 'Image Dir')
-		self.cartridge_assembly_QC['Max Retry'] 		= self.cartridge_assembly_QC_config.getint('GENERAL', 'Max Retry')
+		self.cartridge_assembly_QC              = []
+		self.cartridge_assembly_QC              = {'Prefix': self.cartridge_assembly_QC_config.get('PREFIX', 'Allowed Prefixes').split(';')}
+		self.cartridge_assembly_QC['LogFile']   = self.cartridge_assembly_QC_config.get('PATH', 'Log File')
+		self.cartridge_assembly_QC['ImageDir']  = self.cartridge_assembly_QC_config.get('PATH', 'Image Dir')
+		self.cartridge_assembly_QC['Max Retry'] = self.cartridge_assembly_QC_config.getint('GENERAL', 'Max Retry')
 
 		# app.logger.info(f'Loading Cartridge Assembly QC config: {self.cartridge_assembly_QC}')
 
